c429d22c919ba5c4b9b963f7cf8bd5f231114fdd
jichong <151018920+jichong-tay@users.noreply.github.com>
2024-12-29 18:31:53 +0800

    model generations

---
+++ app/models/item.rb
@@ -0,0 +1,9 @@
+class Item < ApplicationRecord
+  has_many :section_items
+  has_many :sections, through: :section_items
+  has_many :item_modifier_groups
+  has_many :modifier_groups, through: :item_modifier_groups
+
+  # Validation for type
+  validates :type, inclusion: { in: [ "Product", "Component" ], message: "%{value} is not a valid type" }
+end
