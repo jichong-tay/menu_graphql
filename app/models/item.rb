dfb39819ebe884d9947dbfd89d718e7760805edf
jichong <151018920+jichong-tay@users.noreply.github.com>
2024-12-29 21:31:31 +0800

    Update item.rb to disable single Table Inheritance (STI) by default

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,9 @@ class Item < ApplicationRecord
   has_many :sections, through: :section_items
   has_many :item_modifier_groups
   has_many :modifier_groups, through: :item_modifier_groups
+  self.inheritance_column = nil # Disables STI, treating type as a normal column
 
   # Validation for type
   validates :type, inclusion: { in: [ "Product", "Component" ], message: "%{value} is not a valid type" }
+  validates :identifier, :label, :price, presence: true
 end
